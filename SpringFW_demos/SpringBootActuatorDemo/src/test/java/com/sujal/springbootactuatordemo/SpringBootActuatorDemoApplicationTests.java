a606186bdb254af1f3eb7a308973be2b67200f52
SujalKumar <sujal31122005@gmail.com>
2026-07-30 10:21:30 +0530

    Spring Boot Actuator Demo

---
+++ SpringFW_demos/SpringBootActuatorDemo/src/test/java/com/sujal/springbootactuatordemo/SpringBootActuatorDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sujal.springbootactuatordemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootActuatorDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
